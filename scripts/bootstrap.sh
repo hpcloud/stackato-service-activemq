13034b3829dd91971a060dee4a1ebd577dc1a6ce
Matthew <matthewf@activestate.com>
2013-09-10 10:21:35 -0700

    use kato config instead of directly loading
    This makes the service bootstrapping cross-compatible with future
    versions of Stackato

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,18 +11,18 @@ cd /s/vcap/services/activemq && bundle install
 # Copy the stackato configuration files to supervisord
 cp /s/vcap/services/activemq/stackato-conf/activemq_* /s/etc/supervisord.conf.d/
 
-# Install to kato
+# Install processes and roles snippets to kato
 cat /s/vcap/services/activemq/stackato-conf/processes-snippet.yml >> /s/etc/kato/processes.yml
 cat /s/vcap/services/activemq/stackato-conf/roles-snippet.yml >> /s/etc/kato/roles.yml
 
 # Restart supervisord
 start-supervisord
 
-# Install to doozer
-RUBYLIB=kato/lib ruby -e 'require "yaml"; require "kato/doozer"; Kato::Doozer.set_component_config("activemq_node", YAML.load_file("/s/vcap/services/activemq/config/activemq_node.yml"))'
-RUBYLIB=kato/lib ruby -e 'require "yaml"; require "kato/doozer"; Kato::Doozer.set_component_config("activemq_gateway", YAML.load_file("/s/vcap/services/activemq/config/activemq_gateway.yml"))'
+# Install service config files to kato 
+cat /s/vcap/services/activemq/config/activemq_gateway.yml | kato config set activemq_gateway / --yaml
+cat /s/vcap/services/activemq/config/activemq_node.yml | kato config set activemq_node / --yaml
 
-# Add the authentication token to the cloud controller
+# Add the authentication token to kato 
 kato config set cloud_controller builtin_services/activemq/token "0xdeadbeef"
 
 # Add the role and restart kato
